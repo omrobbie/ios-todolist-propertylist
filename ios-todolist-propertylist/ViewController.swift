9022bf388d6bc59f0231bec6dc5c6520704c2fa1
omrobbie <putu.roby@yahoo.com>
2020-06-29 23:08:50 +0800

    add new item and save it to plist

--- ios-todolist-propertylist/ViewController.swift
+++ ios-todolist-propertylist/ViewController.swift
@@ -8,9 +8,17 @@
 
 import UIKit
 
+struct Item: Encodable {
+
+    let title: String
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+
+    private let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last!.appendingPathComponent("Items.plist")
+    private var items = [Item]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,13 +31,25 @@ class ViewController: UIViewController {
         tableView.dataSource = self
     }
 
+    private func saveData(_ value: [Item]) {
+        do {
+            let data = try PropertyListEncoder().encode(value)
+            try data.write(to: self.dataFilePath)
+        } catch {
+            print(error.localizedDescription)
+            return
+        }
+    }
+
     @IBAction func btnAddTapped(_ sender: Any) {
         var textField = UITextField()
 
         let alertVC = UIAlertController(title: "Add new item", message: "", preferredStyle: .alert)
         let actionCancel = UIAlertAction(title: "Cancel", style: .cancel)
         let actionAdd = UIAlertAction(title: "Add", style: .default) { (_) in
-            print(textField.text!)
+            self.items.append(Item(title: textField.text!))
+            self.tableView.reloadData()
+            self.saveData(self.items)
         }
 
         alertVC.addTextField { (alertTextField) in
@@ -47,12 +67,13 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return items.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
-        cell.textLabel?.text = "Item \(indexPath.row)"
+        let item = items[indexPath.row]
+        cell.textLabel?.text = item.title
         return cell
     }
 }
